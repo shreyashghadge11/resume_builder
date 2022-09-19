1c795bd26d3d991d6b56974d2a25b649d2b4fb6d
Shreyash Ghadge <55745865+shreyashghadge11@users.noreply.github.com>
2022-09-19 23:38:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-
+app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
